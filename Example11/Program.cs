c518952fe8af9b620f516d9cca82078bc3d27c73
Luiz Parente <lp.parente@gmail.com>
2023-02-06 20:42:52 -0500

    Static classes.

---
+++ Example11/Program.cs
@@ -0,0 +1,45 @@
+﻿using Example11.Models;
+using Example11.Services;
+using System;
+
+namespace Example11 {
+	class Program {
+		static void Main(string[] args) {
+			var products = ProductService.Get();
+
+			// Existing products.
+			//DisplayProducts(products);
+
+			// Adding new product.
+			var product = new Product("Bike", "A very nice bike.", 500);
+			ProductService.Create(product);
+
+			// Retrieving all products.
+			products = ProductService.Get();
+			//DisplayProducts(products);
+
+			// Retrieving a single product by guid.
+			product = ProductService.Get(product.ProductGuid);
+			//Console.WriteLine(product);
+
+			// Updating product.
+			product.Price = 299;
+			ProductService.Update(product);
+
+			products = ProductService.Get();
+			//DisplayProducts(products);
+
+			// Deleting a product.
+			ProductService.Delete(product);
+
+			products = ProductService.Get();
+			DisplayProducts(products);
+		}
+
+		private static void DisplayProducts(Product[] products) {
+			foreach (var p in products) {
+				Console.WriteLine(p);
+			}
+		}
+	}
+}
---
+++ Example11/Services/ProductService.cs
@@ -0,0 +1,64 @@
+﻿using Example11.Models;
+
+namespace Example11.Services {
+	public static class ProductService {
+		private static Product[] s_products;
+
+		static ProductService() {
+			s_products = new Product[] {
+				new Product("Electric Guitar", "A very nice electric guitar.", 12999),
+				new Product("Acoustic Guitar", "A very nice acoustic guitar.", 8999),
+				new Product("Keyboards", "A very nice keyboards.", 2999),
+				new Product("Drums set", "A very nice drums set.", 7999),
+				new Product("Bass Guitar", "A very nice bass guitar.", 4999),
+			};
+		}
+
+		public static void Create(Product obj) {
+			// Logic to store product in database.
+			Product[] newProducts = new Product[s_products.Length + 1];
+
+			s_products.CopyTo(newProducts, 0);
+			newProducts[newProducts.Length - 1] = obj;
+
+			s_products = newProducts;
+		}
+
+		public static Product[] Get() {
+			// Logic to retrieve all products from database.
+			return s_products;
+		}
+
+		public static Product Get(string guid) {
+			// Logic to retrieve product from database.
+			foreach (var product in s_products) {
+				if (product.ProductGuid == guid) {
+					return product;
+				}
+			}
+
+			return null;
+		}
+
+		public static void Update(Product obj) {
+			// Logic to update product in database.
+			Delete(obj);
+			Create(obj);
+		}
+
+		public static void Delete(Product obj) {
+			// Logic to delete a product from database.
+			Product[] newArray = new Product[s_products.Length - 1];
+			int index = 0;
+
+			foreach (var product in s_products) {
+				if (product.ProductGuid != obj.ProductGuid) {
+					newArray[index] = product;
+					index++;
+				}
+			}
+
+			s_products = newArray;
+		}
+	}
+}
