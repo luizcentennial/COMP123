1c302cda4df24e5c3c5f81d9564d547a09daaf0e
Luiz Parente <31785518+luizparente@users.noreply.github.com>
2023-05-23 20:01:08 -0400

    Updating example 05.

--- Example05/Program.cs
+++ Example05/Program.cs
@@ -3,9 +3,11 @@ using System;
 
 namespace Example05 {
 	class Program {
-		static void Main(string[] args) {
-			Person person = new Person("John", new DateTime(1985, 7, 15)); // <-- This is a constructor call.
-			person.Height = 1.85; // meters
+		static void Main(string[] args) {			
+			// The Person class can now be instantiated in 3 different ways.
+			Person person1 = new Person();
+			Person person2 = new Person("John");
+			Person person3 = new Person("John", new DateTime(1985, 7, 15));
 		}
 	}
 }
