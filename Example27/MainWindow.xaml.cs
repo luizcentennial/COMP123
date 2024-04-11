61429fb235f1128ed82f7a30edee7831716073f4
Luiz Parente <31785518+luizparente@users.noreply.github.com>
2024-04-10 22:56:30 -0400

    Updating Example 27.

--- Example27/MainWindow.xaml.cs
+++ Example27/MainWindow.xaml.cs
@@ -1,26 +1,30 @@
-﻿using System;
-using System.Windows;
+﻿using System.Windows;
 
-namespace Example27 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window {
-		public MainWindow() {
-			InitializeComponent();
-		}
+namespace Example27
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
 
-		private void OnSignIn(object sender, EventArgs e) {
-			// Named controls can be accessed in the code-behind like any other class field.
-			// Since txtName is a TextBox, we can retrieve its current value from property 'Text'.
-			string name = txtName.Text;
+        private void OnSignIn(object sender, EventArgs e)
+        {
+            // Named controls can be accessed in the code-behind like any other class field.
+            // Since txtName is a TextBox, we can retrieve its current value from property 'Text'.
 
-			if (!string.IsNullOrWhiteSpace(name)) {
-				MessageBox.Show($"Login successful with username {name}!", "Success!", MessageBoxButton.OK, MessageBoxImage.Information);
-			}
-			else {
-				MessageBox.Show("Username not provided.", "Error.", MessageBoxButton.OK, MessageBoxImage.Error);
-			}
-		}
-	}
+            string username = this.txtUsername.Text;
+            string password = this.pwdPassword.Password; // <-- NO! This is terribly insecure. 
+
+            if (username == "john" && password == "123")
+            {
+                MessageBox.Show("Login successful!", "Success!", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Invalid credentials.", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
 }
